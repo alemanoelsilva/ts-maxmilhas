aeac90b4af5381d29fc018c3458dbd6c470118d6
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-03 11:18:56 -0300

    test: ensure AddBlacklist Controller succeeds

---
+++ src/presentation/controllers/add-blacklist.spec.ts
@@ -0,0 +1,22 @@
+import { AddBlacklist } from './add-blacklist'
+
+describe('Controller - Add Blacklist', () => {
+  describe('Successful blacklist creation', () => {
+    it('should return status code 201 when AddBlacklist succeeds', () => {
+      const addBlacklist = new AddBlacklist()
+
+      const request = {
+        body: {
+          documentNumber: '999.999.999-99'
+        }
+      }
+
+      const response = addBlacklist.handler(request)
+
+      expect(response).toEqual({
+        message: 'Document was created with success',
+        statusCode: 201
+      })
+    })
+  })
+})
