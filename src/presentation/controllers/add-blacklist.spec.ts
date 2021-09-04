f6288a012d75b8503f7c4861176a063d335491bf
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-03 23:36:25 -0300

    test: ensure request and response follow the http protocol

--- src/presentation/controllers/add-blacklist.spec.ts
+++ src/presentation/controllers/add-blacklist.spec.ts
@@ -1,20 +1,23 @@
 import { AddBlacklist } from './add-blacklist'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 describe('Controller - Add Blacklist', () => {
   describe('Successful blacklist creation', () => {
     it('should return status code 201 when AddBlacklist succeeds', () => {
       const addBlacklist = new AddBlacklist()
 
-      const request = {
+      const request: HttpRequest = {
         body: {
           documentNumber: '999.999.999-99'
         }
       }
 
-      const response = addBlacklist.handler(request)
+      const response: HttpResponse = addBlacklist.handler(request)
 
       expect(response).toEqual({
-        message: 'Document was created with success',
+        body: {
+          message: 'Document was created with success'
+        },
         statusCode: 201
       })
     })
@@ -24,14 +27,16 @@ describe('Controller - Add Blacklist', () => {
     it('should return status code 400 when document number is not provided', () => {
       const addBlacklist = new AddBlacklist()
 
-      const request = {
+      const request: HttpRequest = {
         body: {}
       }
 
-      const response = addBlacklist.handler(request)
+      const response: HttpResponse = addBlacklist.handler(request)
 
       expect(response).toEqual({
-        message: 'Document was not provided',
+        body: {
+          message: 'Document was not provided'
+        },
         statusCode: 400
       })
     })
