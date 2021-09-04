2649f26c0c266dd0bdc94674849462d99ce26599
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-04 20:03:41 -0300

    feat: ensure AddBlacklistController return 201 when document number is valid

--- src/presentation/controllers/add-blacklist.ts
+++ src/presentation/controllers/add-blacklist.ts
@@ -2,15 +2,26 @@ import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { IController } from '../protocols/controller'
 import { badRequest, succeed } from '../helpers/http/http-helpers'
 import { MissingParamError } from '../errors/missing-params-error'
+import { IValidationAdapter } from '../protocols/validation-adapter'
 
 export class AddBlacklist implements IController {
+  constructor(private readonly documentNumberValidation: IValidationAdapter) {}
+
   async handler(request: IHttpRequest): Promise<IHttpResponse> {
     const { documentNumber } = request.body
 
     if (!documentNumber) {
       const missingParamError = new MissingParamError('documentNumber')
-
       return new Promise(resolve => resolve(badRequest(missingParamError)))
+    }
+
+    if (!this.documentNumberValidation.validate(documentNumber)) {
+      return new Promise(resolve => resolve(succeed({
+        body: {
+          message: 'Document provided is not valid'
+        },
+        statusCode: 401
+      })))
     }
 
     return new Promise(resolve => resolve(succeed({
