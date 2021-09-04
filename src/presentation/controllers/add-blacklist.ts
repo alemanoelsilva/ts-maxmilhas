dace068aa326ffa27f11c35263304b4c91d4cd9d
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2021-09-03 23:36:10 -0300

    feat: ensure request and response follow the http protocol

--- src/presentation/controllers/add-blacklist.ts
+++ src/presentation/controllers/add-blacklist.ts
@@ -1,14 +1,20 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class AddBlacklist {
-  handler(request: any): any {
+  handler(request: HttpRequest): HttpResponse {
     if (request.body.documentNumber) {
       return {
-        message: 'Document was created with success',
+        body: {
+          message: 'Document was created with success'
+        },
         statusCode: 201
       }
     }
 
     return {
-      message: 'Document was not provided',
+      body: {
+        message: 'Document was not provided'
+      },
       statusCode: 400
     }
   }
